634232fe1b55602fc50c33dde4286918cd2c406b
DanMaklen <omarnasr@aucegypt.edu>
2019-02-16 00:06:27 +0200

    Clean up

--- Compiler/src/Main.cpp
+++ Compiler/src/Main.cpp
@@ -3,32 +3,6 @@
 #include "AST.hpp"
 #include "ASTFileWriter.hpp"
 
-long long eval(const AST* ast){
-	switch(ast->GetASTNodeType()){
-	case ASTNodeType::EXPRESSION:{
-		const Expression* expr = dynamic_cast<const Expression*>(ast);
-		switch(expr->GetOperation()){
-		case Expression::Operation::ADDITION:
-			return eval(ast->GetChildren()[0]) + eval(ast->GetChildren()[1]);
-		case Expression::Operation::SUBTRACTION:
-			return eval(ast->GetChildren()[0]) - eval(ast->GetChildren()[1]);
-		case Expression::Operation::MULTIPLICATION:
-			Logger::Log(Logger::Type::INFO, eval(ast->GetChildren()[1]));
-			return eval(ast->GetChildren()[0]) * eval(ast->GetChildren()[1]);
-		case Expression::Operation::DIVISION:
-			return eval(ast->GetChildren()[0]) / eval(ast->GetChildren()[1]);
-		case Expression::Operation::REMAINDER:
-			return eval(ast->GetChildren()[0]) % eval(ast->GetChildren()[1]);
-		}
-	}
-	case ASTNodeType::INTEGER_LITERAL:{
-		const IntegerLiteral* val = dynamic_cast<const IntegerLiteral*>(ast);
-		return val->value.int64;
-	}
-	}
-	return 0;
-}
-
 int main(int argc, const char * argv[]){
 	// TODO: Rewrite argument parser
 	if(argc < 2){
